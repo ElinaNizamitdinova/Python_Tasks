fad83c7c3c0f81e8401965ea982967db807c87a1
Elina <elina.eldarovna.spb@gmail.com>
2024-01-20 16:57:02 +0300

    PB

--- directory.py/controllee.py
+++ directory.py/controllee.py
@@ -1,5 +1,12 @@
 import text
 import view
+import model
+
+
+def find_contact():
+     word = view.input_data(text.input_seash_word)
+     result = model.find_contact(word)
+     view.show_contact(result,text.contact_not_found(word))
 
 
 def start_app():
@@ -7,18 +14,33 @@ def start_app():
         choice = view.main_menu()
         match choice:
             case 1:
-                pass
+               model.open_file()
+               view.print_messege(text.empty_phone_book)
             case 2:
-                pass
+               model.save_file()
+               view.print_messege(text.save_successful)
             case 3:
-                pass
+               pb=model.phone_book
+               view.show_contact(pb,text.empty_phone_book)
             case 4:
-                pass
+               contact = view.add_contact(text.new_contact)
+               model.new_contact(contact)
+               view.print_messege(text.new_contact_added_successful(contact[0]))
             case 5:
-                pass
+               find_contact()
             case 6:
-                pass
+               find_contact()
+               pb = model.phone_book
+               c_id = int(view.input_data(text.input_id_change_contact()))
+               c_contact = view.add_contact(text.change_contact,pb[c_id])
+               model.change_contact(c_id,c_contact)
+               view.print_messege(text.contact_changed_successful(c_contact[0]))
             case 7:
-                pass
+               find_contact()
+               pb = model.phone_book
+               c_id = int(view.input_data(text.input_id_delete_contact()))
+               name = model.delete_contact(c_id)[0]
+               view.print_messege(text.contact_delete_successful(name ))
             case 8:
-                pass
+               view.print_messege(text.good_bye)
+               break
--- directory.py/view.py
+++ directory.py/view.py
@@ -15,3 +15,34 @@ def main_menu()->int:
             print(f"Введите пункт меню от 1 до {len(text.main_menu-1)}")
 
 
+def show_contact(p_book:dict[int,list[str]],error_messege:str):
+    max_size=list(map(lambda x: len(max(x,key=len)),list(zip(*p_book.values()))))
+
+    if p_book:
+        print("\n"+"="*(sum(max_size)+7))
+        for n, contact in p_book.items():
+            print(f"{n:>3}.{contact[0]:< {max_size[0]+1}} {contact[1]:< {max_size[1]+1}} {contact[2]:< {max_size[2]+1}}")
+        print((sum(max_size)+7)+"\n")
+    else:
+        print_messege(error_messege)
+
+def print_messege(messege: str):
+    print("\n"+"="*len(messege))
+    print(messege)
+    print("="*len(messege)+"\n")
+
+
+def add_contact(messege:list[str],contact:list[str]= None):
+    contact = contact if contact else ["","",""]
+    for n,mes in enumerate(messege):
+        field=input(mes)
+        contact[n] = field if field else contact[n]
+    return contact
+
+
+def input_data(messege: str) -> str:
+    return input(messege)
+
+
+
+
